2ec3872fcdf75cc7b14f8453620def60d0f79d47
Janak <vidurinda@gmail.com>
2022-03-19 09:49:05 +0530

    Initial commit

---
+++ src/main/java/com/ices/AzureTest/AzureTestApplication.java
@@ -0,0 +1,13 @@
+package com.ices.AzureTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ices/AzureTest/AzureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ices.AzureTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
